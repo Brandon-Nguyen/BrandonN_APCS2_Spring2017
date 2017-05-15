4b242ab36fd5b6125e9c9a3a25d4377923fbbfdf
Brandon B. Nguyen <18nguyenb20@millshighsch.org>
2017-05-15 09:46:47 -0700

    idk something

---
+++ TapOfWar/src/Images.java
@@ -0,0 +1,39 @@
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
+public class Images extends JLabel {
+
+	public Images() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Images(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Images(Icon arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Images(String arg0, int arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Images(Icon arg0, int arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Images(String arg0, Icon arg1, int arg2) {
+		super(arg0, arg1, arg2);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void background(){
+		
+	}
+
+}

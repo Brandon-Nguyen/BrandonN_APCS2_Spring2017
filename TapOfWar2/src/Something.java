36b4de72350f1669dc9b9fd1de4bfd42d6bf0fa2
Brandon B. Nguyen <18nguyenb20@millshighsch.org>
2017-05-15 09:47:17 -0700

    idk

--- TapOfWar2/src/Something.java
+++ TapOfWar2/src/Something.java
@@ -1,13 +1,9 @@
 
-public class Something {
-
-	public Something() {
-		// TODO Auto-generated constructor stub
-	}
-
+public class Something extends JFrame {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame("Hello there!");
+		
 	}
 
 }

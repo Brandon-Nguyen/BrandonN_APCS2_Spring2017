0bbe1debc72f1ff2544717177ef547567ab2bd41
Brandon B. Nguyen <18nguyenb20@millshighsch.org>
2017-05-18 23:17:07 -0700

    got the images to all work i think, all in frame

--- TapOfWar2/src/Frame.java
+++ TapOfWar2/src/Frame.java
@@ -10,19 +10,39 @@ public class Frame extends JFrame {
 	}
 	
 	public Frame(){
-
-		setTitle("Test Paint");
-		setSize(400,400);
+		
+		// title and size of the window
+		setTitle("TAP OF WAR!");
+		setSize(1920,1080);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
 		
 		init();
+
 	}
 	
 	public void init(){
-		setLocationRelativeTo(null);
-		setLayout(new GridLayout(1,1,0,0));
-		s = new Something();
-		add(s);
+		//placing the images and scene on the frame
+
+		setLayout(null);
+		
+		JButton button = new JButton("START");
+        button.setBounds(910,470,100,30);
+        add(button);
+        
+        JLabel label = new JLabel("TAP OF WAR");
+        label.setFont(label.getFont().deriveFont(Font.BOLD, 48));
+        label.setForeground(Color.GRAY);
+        label.setBounds(810,0,500,100);
+        add(label);
+        
+        JLabel label2 = new JLabel(new ImageIcon(getClass().getResource("/tug.png")));
+        label2.setBounds(720,675,480,305);
+        add(label2);
+        
+        JLabel background = new JLabel(new ImageIcon(getClass().getResource("/flowers.gif")));
+        background.setBounds(0,0,1920,1080);
+        add(background);
 		
 		setVisible(true);
 	}
--- TapOfWar2/src/Something.java
+++ TapOfWar2/src/Something.java
@@ -1,9 +1,13 @@
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.Color;
+
 
 public class Something extends JPanel{
 	private BufferedImage image;
@@ -11,20 +15,25 @@ public class Something extends JPanel{
 	
 	public Something(){
 		try{
-			image = ImageIO.read(getClass().getResource("/gordon.jpg"));
+			image = ImageIO.read(getClass().getResource("/flowers.gif"));
 			image2 = ImageIO.read(getClass().getResource("/tug.png"));
 		}catch(IOException e){
 			e.printStackTrace();
 		}
-		
+        
+        
 		repaint();
 		
 		
 	}
 	
-	public void paint(Graphics g){
-		g.drawImage(image, 0, 0, null);
+	public void paintComponents(Graphics g){
+		g.setFont(new Font("Arial", Font.BOLD, 48));
+		g.drawString("TAP OF WAR", 900, 50);
+		g.setColor(new Color(91,90,90));
+		//g.drawImage(image, 0, 0, null);
 		g.drawImage(image2, 0, 0, null);
+
 
 	}
 }
